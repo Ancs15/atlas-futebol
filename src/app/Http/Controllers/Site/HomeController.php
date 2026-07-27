939a1a16f34b1282d5e705803d713958b6fcc115
AnCS15 <mundoprehistocico@gmail.com>
2026-07-27 17:29:49 -0300

    Atualização dos Banners do ATLAS FUTEBOL de acordo com o Banco de Dados [INCOMPLETO]

--- src/app/Http/Controllers/Site/HomeController.php
+++ src/app/Http/Controllers/Site/HomeController.php
@@ -4,13 +4,20 @@
 namespace App\Http\Controllers\Site;
 //  CHAMA O ARQUIVO CONTROLLER
 use App\Http\Controllers\Controller;
+// CHAMA O ARQUIVO BANNER
+use App\Models\Banner;
 
 // CRIA A CLASSE HOME CONTROLLER
 class HomeController extends Controller {
 
     //CHAMA A PÁGINA PRINCIPAL
     public function home() {
-        return view('site.home.home');
+
+        // CHAMA O ARQUIVO BANNER
+        $listaBanners = Banner::where('status_banner', 'ATIVO')->get();
+
+        // RETORNA A PÁGINA HOME COM A LISTA DE BANNERS
+        return view('site.home.home', compact('listaBanners'));
     }
 
 }
---
+++ src/app/Models/Banner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\DAtabase\Eloquent\Model;
+
+class Banner extends Model {
+    protected $table = 'tbl_banner';
+    protected $primaryKey = 'id_banner';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'titulo_banner',
+        'subtitulo_banner',
+        'nome_img_banner',
+        'link_banner',
+        'texto_botao1_banner',
+        'link_botao1_banner',
+        'texto_botao2_banner',
+        'link_botao2_banner',
+        'status_banner',
+    ];
+}
