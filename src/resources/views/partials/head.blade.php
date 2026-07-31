d005311c29d1a0be1892833b4e85e0e283ae91f5
AnCS15 <mundoprehistocico@gmail.com>
2026-07-31 14:49:23 -0300

    ícones FAVICONS na head do site

--- src/resources/views/partials/head.blade.php
+++ src/resources/views/partials/head.blade.php
@@ -3,6 +3,28 @@
 
     <title>Atlas - Formando Estrelas</title>
 
+    <!-- ÍCONES FLAVICON -->
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('atlas/upload/icones-atlas/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('atlas/upload/icones-atlas/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('atlas/upload/icones-atlas/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('atlas/upload/icones-atlas/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('atlas/upload/icones-atlas/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('atlas/upload/icones-atlas/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('atlas/upload/icones-atlas/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('atlas/upload/icones-atlas/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('atlas/upload/icones-atlas/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('atlas/upload/icones-atlas/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('atlas/upload/icones-atlas/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('atlas/upload/icones-atlas/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('atlas/upload/icones-atlas/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('atlas/upload/icones-atlas/manifest.json') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('atlas/upload/icones-atlas/ms-icon-144x144.png') }}">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- LINKS DE ESTILO -->
+
     <link rel='stylesheet' href='{{ asset("atlas/css/font-awesome.css") }}' type='text/css' media='all'>
     <!-- <link rel='stylesheet' href='atlas/css/elegant/elegant-font.css' type='text/css' media='all'> -->
     <link rel='stylesheet' href='{{ asset("atlas/css/style.css") }}' type='text/css' media='all'>
