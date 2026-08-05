29ce8bde07a6fc9f97c3d2163b8def7124c88fa0
AnCS15 <mundoprehistocico@gmail.com>
2026-08-05 17:16:21 -0300

    Logo Din

--- src/app/Http/Controllers/Site/ContatoController.php
+++ src/app/Http/Controllers/Site/ContatoController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Logo;
 Class ContatoController extends Controller {
 
     public function contato() {
 
-        return view('site.contato.contato');
+        //CHAMA O ARQUIVO LOGO
+        $LogoAtiva = Logo::where('status_logo', 'ATIVO')->first();
+        // dd($LogoAtiva);
+
+        return view('site.contato.contato', compact('LogoAtiva'));
 
     }
 
--- src/app/Http/Controllers/Site/SobreController.php
+++ src/app/Http/Controllers/Site/SobreController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Logo;
 use App\Models\Parceiros;
 
 Class SobreController extends Controller {
     public function sobre()
     {
 
+        //CHAMA O ARQUIVO LOGO
+        $LogoAtiva = Logo::where('status_logo', 'ATIVO')->first();
+        // dd($LogoAtiva);
+
         $listaParceiros = Parceiros::where('status_parceiros', 'ATIVO')->orderBy('nome_parceiros', 'ASC')->get();
         //dd($listaParceiros);        
 
-        return view('site.sobre.sobre', compact('listaParceiros'));
+        return view('site.sobre.sobre', compact('listaParceiros', 'LogoAtiva'));
     }
 }
